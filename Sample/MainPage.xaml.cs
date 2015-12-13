5f0e45b962ac04facac2d89693e805faf148e8d3
HIGAN <higan@live.cn>
2015-12-13 21:07:47 +0800

    - Add Nuget Package Metadata

--- Sample/MainPage.xaml.cs
+++ Sample/MainPage.xaml.cs
@@ -20,12 +20,7 @@ namespace Sample
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var v3 = new Vector3(1, 2, 3);
-            var m4x4 = new Matrix4x4(11, 12, 13, 14, 21, 22, 23, 24, 31, 32,33, 34, 41, 42, 43, 44);
-
-            CompositionProperties.SetOffset(TargetRect,v3);
-            CompositionProperties.SetTransformMatrix(TargetRect,m4x4);
-
+            
             (this.Resources["Storyboard"] as Storyboard).Start();
         }
     }
